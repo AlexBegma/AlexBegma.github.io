a10814643468dfcc70cb4d9e2fcf5055093a284b
alexbehma <alexbegma@gmail.com>
2019-03-31 18:49:26 +0300

    del

---
+++ akad/src/js/script.js
@@ -0,0 +1,22 @@
+// var userName = ("Введите имя!", "Макс");
+// var isTeaWanted = confirm('Вы чаю хотите?');
+
+// alert('Посетитель:' + userName);
+// alert('Чай:' + isTeaWanted);
+
+
+
+// ;(function IIFE(){
+
+// console.log('Hello!');
+
+// const text = 'I love BMW';
+
+// document.write('Very good!');
+
+
+
+// })()
+
+
+
