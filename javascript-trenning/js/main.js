75c27c3e936485a6dfeed019eaf529aaf52e1724
alexbehma <alexbegma@gmail.com>
2019-04-03 20:22:45 +0300

    modified

---
+++ javascript-trenning/js/main.js
@@ -0,0 +1,38 @@
+// var userName = ("Введите имя!", "Макс");
+// var isTeaWanted = confirm('Вы чаю хотите?');
+
+// alert('Посетитель:' + userName);
+// alert('Чай:' + isTeaWanted);
+
+
+
+;(function IIFE(){
+
+console.log('Hello!');
+
+const text = 'I love BMW';
+
+document.write('Very good!');
+
+})()
+
+
+
+var vegetable={name:'буряк', qty:2};
+
+var message='Нам нужны овощи: ';
+  message+=vegetable.name;
+  message+=', в количестве: ';
+  message+=vegetable.qty;
+  message+=' штук.';
+
+  document.write(message);
+
+
+  ;(function(){
+  	'use strict';
+
+  	var visitorName = prompt('Как вас зовут?', "Миша");
+  	var message = 'Привет!';
+
+  })();
