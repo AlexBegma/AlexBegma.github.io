bb29c5e28b5cf8ed095dbd4fb999918aa9223b84
alexbehma <alexbegma@gmail.com>
2019-05-07 12:28:41 +0300

    create site before blog

--- bem-bootstrap/js/main.js
+++ bem-bootstrap/js/main.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-	$('.button[filter]').click(function(){
-		if($(this).attr('filter')=='all'){
-			if($(this).attr('val')=='off'){
-				$('.button[filter]').attr('val', 'off');
-				$(this).attr('val', 'on');
-				$('.button[filter]').removeClass('focused');
-				$(this).addClass('focused');
-				$('.filter > div').show(300);
-			}
-		} else
-		if($(this).attr('val')=='off'){
-			$('.button[filter]').attr('val', 'off');
-			$(this).attr('val', 'on');
-			$('.button[filter]').removeClass('focused');
-			$(this).addClass('focused');
-			$('.filter > div').hide(300);
-			var filter = $(this).attr('filter');
-			$('.filter > div[filter='+filter+']').show(300);
-		}
-	})
-})
+// $(document).ready(function() {
+// 	$('.button[filter]').click(function(){
+// 		if ($(this).attr('val')=='off'){
+// 			$('.button[filter]').attr('val', 'off').removeClass('focused');
+// 			$(this).attr('val', 'on').addClass('focused');
+// 			$('.filter > div').hide(300);
+// 			$('.filter > div[filter=' + $(this).attr('filter') + ']').show(300);
+// 			if ($(this).attr('filter') == 'all') {
+// 				$('.button[filter]').attr('val', 'off').removeClass('focused');
+// 				$(this).attr('val', 'on').addClass('focused');
+// 				$('.filter > div').show(300);
+// 			}
+// 		}
+// 	})
+// 	$('.multiple-items').slick({
+// 		infinite: true,
+// 		slidesToShow: 3,
+// 		slidesToScroll: 3,
+// 		arrows: false,
+// 		dots: true
+// 	});
+// })
