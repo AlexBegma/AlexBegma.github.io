ddd80f76ae425743447320102d63771a7fd074fd
alexbehma <alexbegma@gmail.com>
2019-05-20 22:11:52 +0300

    commits 20.05.2019

--- bem-bootstrap/js/main.js
+++ bem-bootstrap/js/main.js
@@ -17,6 +17,41 @@
 // 		slidesToShow: 3,
 // 		slidesToScroll: 3,
 // 		arrows: false,
-// 		dots: true
+// 		dots: true,
+// 		dotsClass: 'dots-style',
+// 		responsive: [{
+// 			breakpoint: 1024,
+// 			settings: {
+// 				slidesToShow: 2,
+// 				slidesToScroll: 2,
+// 			}
+// 		}]
 // 	});
+
+// 	// Меню
+	$('ul.menu a[href^_"#"').click(function (){
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+			}, 500);
+		$('ul.menu a[href^-"#"').css({
+			'color': '#004bee'
+		});
+		return false;
+	});
+	// Выпадающее меню
+	$('.menu-icon').click(function(){
+		$('nav').slideToggle(500);
+	});
+	// Кнопка наверх
+	$(window).scroll(function() {
+		if ($(this).scrollTop() !-0)
+			$('#toTop').fadeIn();
+		else $('#toTop').fadeOut();
+	});
+	$('#toTop').click(function() {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+	});
 // })
