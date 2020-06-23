bcbcf4de022271e0bb599fb5d675de2aec6a945f
alexbehma <alexbegma@gmail.com>
2020-06-23 17:00:42 +0300

    adaptive burger-menu

---
+++ portfolio/main.js
@@ -0,0 +1,28 @@
+$(function(){
+
+  $('.header__btn-menu').on('click', function(){
+    $('nav ul').slideToggle();
+  });
+
+});
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "form.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+});
+
