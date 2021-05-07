34c961eb8ab8c6f0ade696623860dbc96afb1ba5
alexbehma <alexbegma@gmail.com>
2021-05-07 08:38:34 +0300

    restart

--- test Zai/js/main.js
+++ test Zai/js/main.js
@@ -31,6 +31,37 @@ $(".slider-nav").slick({
   focusOnSelect: true,
 });
 
+
+$(".center").slick({
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+
+
+
+
 //  $(function () {
 //     $('.slider-for').each(function(num, elem) {
 //         elem = $(elem);
