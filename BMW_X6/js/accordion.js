910e1cd4cc39697b8c5bac5c45d51c293d28f982
alexbehma <alexbegma@gmail.com>
2021-05-18 22:06:59 +0300

    accordion on JS

---
+++ BMW_X6/js/accordion.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+ const featureLinkElems = document.querySelectorAll(".feature__link");
+ const featureSubElems = document.querySelectorAll(".feature-sub");
+
+ //  for (let i = 0; i < featureLinkElems.length; i++) {
+ //   featureLinkElems[i].addEventListener("click", () => {
+ //    featureSubElems[i].classList.toggle('hidden');
+ // featureLinkElems[i].classList.toggle("feature__link_active");
+ //    });
+ //  }
+
+ featureLinkElems.forEach((btn, index) => {
+
+  btn.addEventListener('click', () => {
+   featureSubElems.forEach((featureSubElem) => {
+    featureSubElem.classList.add('hidden')
+   });
+   featureLinkElems.forEach((featureLinkElem) => {
+    featureLinkElem.classList.remove("feature__link_active");
+   });
+   featureSubElems[index].classList.remove('hidden');
+   btn.classList.add('feature__link_active');
+  });
+
+ });
+
+ // featureLinkElems.forEach((btn, index) => {
+ //   btn.addEventListener("click", () => {
+
+ //     featureSubElems[index].classList.toggle("hidden");
+ //     btn.classList.toggle("feature__link_active");
+ //   });
+ // });
+
+});
