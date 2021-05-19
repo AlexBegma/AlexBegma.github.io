0b69c1fd01063b8fbd23e58b37268858429a882a
alexbehma <alexbegma@gmail.com>
2021-05-19 16:31:03 +0300

    smooth scroll; tabs

--- BMW_X6/js/accordion.js
+++ BMW_X6/js/accordion.js
@@ -32,4 +32,22 @@ document.addEventListener("DOMContentLoaded", () => {
  //   });
  // });
 
+   // for (let i = 0; i < featureLinkElems.length; i++) {
+   //      featureLinkElems[i].addEventListener('click', () => {
+   //          if(featureLinkElems[i].classList.contains('feature__link_active')) {
+   //              featureLinkElems[i].classList.remove('feature__link_active')
+   //              featureSubElems[i].classList.add('hidden')
+   //          } else {
+   //              featureLinkElems.forEach((featureLinkElem) => {
+   //                  featureLinkElem.classList.remove('feature__link_active')
+   //              })
+   //              featureSubElems.forEach((featureSubElem) => {
+   //                  featureSubElem.classList.add('hidden')
+   //              })
+   //              featureLinkElems[i].classList.add('feature__link_active')
+   //              featureSubElems[i].classList.remove('hidden')
+   //          }
+   //      })
+   //  }
+
 });
