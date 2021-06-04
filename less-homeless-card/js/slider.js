d471a88f6de24eda83e0ff6f32683cbed4c18f91
alexbehma <alexbegma@gmail.com>
2021-06-04 22:56:57 +0300

    desktop version

--- less-homeless-card/js/slider.js
+++ less-homeless-card/js/slider.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
-  $(".slider-for").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-   arrows: true,
-   dots: false,
-    fade: true,
-    asNavFor: ".slider-nav"
-  });
-  $(".slider-nav").slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: true,
-    centerMode: false,
-    focusOnSelect: true,
-    asNavFor: ".slider-for"
-  });
+ $(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+  fade: true,
+  asNavFor: ".slider-nav"
+ });
+ $(".slider-nav").slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  centerMode: false,
+  focusOnSelect: true,
+  asNavFor: ".slider-for"
+ // responsive: [
+ //   {
+ //    breakpoint: 1024,
+ //    settings: {
+ //     slidesToShow: 2,
+ //    },
+ //   },
+ //   {
+ //    breakpoint: 480,
+ //    settings: {
+ //     slidesToShow: 1,
+ //    },
+ //   },
+ //  ]; 
+ });
 });
