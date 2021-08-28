2753dc32b79e88fba780902c23fbf5c1525fd3f2
alexbehma <alexbegma@gmail.com>
2021-08-28 17:40:38 +0300

    AJAX

--- Wona_test/main.js
+++ Wona_test/main.js
@@ -44,4 +44,50 @@ $(document).ready(function () {
     $(".header__burger, .header__menu").toggleClass("active");
     $("body").toggleClass("lock");
   });
+
+ $.ajax({
+   type: "GET",
+   url: "https://www.wona.co.il/test_8192735.php",
+   success: function (data, status) {
+     for (i = 0; i < data.length; i++) {
+       var element = data[i];
+
+       var image = $(
+         '<a class="content__link"><img class="content__image" src="' +
+           element.image +
+           '" alt="image"></a>'
+       );
+
+       var productTitle = $(
+         '<div class="product-title">' + element.title + "</div>"
+       );
+
+       var productExcerpt = $('<div class="product-excerpt"></div>');
+       productExcerpt.append('<span class="excerpt__info">םיעבצ4</span>');
+       productExcerpt.append(
+         '<span class="excerpt__price">₪' + element.price + "</span>"
+       );
+
+       var productAction = $('<div class="product-action"></div>');
+       productAction.append(
+         '<a href="' +
+           element.link +
+           '" class="product-action__link">סקירה מהירה</a>'
+       );
+
+       var productDetails = $('<div class="product-details"></div>');
+       productDetails.append(productTitle);
+       productDetails.append(productExcerpt);
+       productDetails.append(productAction);
+
+       var content = $('<li class="content"></li>');
+       content.append(image);
+       content.append(productDetails);
+
+       $(".products").append(content);
+     }
+   },
+   dataType: "json",
+ });
+
 });
