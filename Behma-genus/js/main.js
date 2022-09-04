2a4e829b431ba3df2250944f2725d447f80f0dd3
alexbehma <alexbegma@gmail.com>
2022-09-04 08:45:27 +0300

    4.08.22

--- Behma-genus/js/main.js
+++ Behma-genus/js/main.js
@@ -4,3 +4,35 @@ $(document).ready(function () {
     $('body').toggleClass('lock');
   });
 });
+
+function readMore() {
+  var dots = document.getElementById('dots');
+  var more = document.getElementById('more');
+  var btn = document.getElementById('btn');
+
+  if (dots.style.display === 'none') {
+    dots.style.display = 'inline';
+    more.style.display = 'none';
+    btn.innerHTML = 'Читать дальше ...';
+  } else {
+    dots.style.display = 'none';
+    more.style.display = 'inline';
+    btn.innerHTML = 'Вернуться';
+  }
+}
+
+function readMore_2() {
+  var dots = document.getElementById('dots-2');
+  var more = document.getElementById('more-2');
+  var btn = document.getElementById('btn-2');
+
+  if (dots.style.display === 'none') {
+    dots.style.display = 'inline';
+    more.style.display = 'none';
+    btn.innerHTML = 'Читать дальше ...';
+  } else {
+    dots.style.display = 'none';
+    more.style.display = 'inline';
+    btn.innerHTML = 'Вернуться';
+  }
+}
