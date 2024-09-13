bda6bc3869d7c99094ad3c0fd2de7d66df2efd93
Aleksandr Behma <alexbegma@gmail.com>
2024-09-13 20:26:07 +0300

    modal windows added

--- huge-search_hw/main.js
+++ huge-search_hw/main.js
@@ -89,3 +89,106 @@ modalController({
   btnOpen: ".section__button2",
   btnClose: ".modal__close",
 });
+
+modalController({
+  modal: ".modal3",
+  btnOpen: ".section__button3",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal4",
+  btnOpen: ".section__button4",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal5",
+  btnOpen: ".section__button5",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal6",
+  btnOpen: ".section__button6",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal7",
+  btnOpen: ".section__button7",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal8",
+  btnOpen: ".section__button8",
+  btnClose: ".modal__close",
+});
+
+
+modalController({
+  modal: ".modal9",
+  btnOpen: ".section__button9",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal10",
+  btnOpen: ".section__button10",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal11",
+  btnOpen: ".section__button11",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal12",
+  btnOpen: ".section__button12",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal13",
+  btnOpen: ".section__button13",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal14",
+  btnOpen: ".section__button14",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal15",
+  btnOpen: ".section__button15",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal16",
+  btnOpen: ".section__button16",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal17",
+  btnOpen: ".section__button17",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal18",
+  btnOpen: ".section__button18",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal19",
+  btnOpen: ".section__button19",
+  btnClose: ".modal__close",
+});
